8bd424827b1138a514ced7e80782b3d0e32121ba
Rajesh <rajeshranjan8271@gmail.com>
2024-08-13 15:47:59 +0530

    fixed bug

--- EncodedCharacters.js
+++ EncodedCharacters.js
@@ -2,8 +2,8 @@ const inputStr = "rajesh";
 const shiftByPos = 4;
 const getEncodedStr = (str, shiftByPos) => {
   let encodedStr = "";
-  let currentPos = 0,
-    newPos = 0;
+  let currentPos = 0;
+  let newPos = 0;
   for (let i = 0; i < str.length; i++) {
     currentPos = str.charCodeAt(i);
     newPos = currentPos + shiftByPos;
